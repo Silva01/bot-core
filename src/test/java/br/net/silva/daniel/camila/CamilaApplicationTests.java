4050228cffa78bf1d62071aab55c0988a7478571
silva01 <silva20102@gmail.com>
2021-08-02 20:59:29 -0300

    Initial Commit

---
+++ src/test/java/br/net/silva/daniel/camila/CamilaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.silva.daniel.camila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f30135f753ce5fda1116f6bd43770d434a7c347f
silva01 <silva20102@gmail.com>
2021-08-10 22:14:31 -0300

    #10 - Adicionado Starter do bot na classe principal do Spring

--- src/main/java/br/net/silva/daniel/core/bot/CoreBotApplication.java
+++ src/main/java/br/net/silva/daniel/core/bot/CoreBotApplication.java
@@ -1,13 +1,35 @@
 package br.net.silva.daniel.core.bot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@Slf4j
 public class CoreBotApplication {
+
+    @Autowired
+    private BotConfig botConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(CoreBotApplication.class, args);
+    }
+
+    @PostConstruct
+    private void botExecution(){
+        try {
+            log.info("Iniciando Bot ");
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(botConfig);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
 }
